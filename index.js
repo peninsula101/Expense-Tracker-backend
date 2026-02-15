f42ab7f1018d554fbe9621fb4f6f3f37fd26264a
TVSCSK <108569220+peninsula101@users.noreply.github.com>
2026-02-15 21:27:39 +0530

    Use environment variable for server port

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const transactionRoute = require('./routes/transactions');
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successful"))
@@ -18,4 +19,6 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/transactions', transactionRoute);
 
-app.listen(5000, () => { console.log("Backend server is running!"); });
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}`);
+});
